787069973904a0e6e38a3d97355b2b724b67558b
ssingh <ssingh@amplify.com>
2015-07-30 17:16:44 -0400

    Clean up and refactoring

--- client/dist/navview.js
+++ client/dist/navview.js
@@ -2,79 +2,7 @@
  * Created by ssingh on 7/29/15.
  */
 
-ko.bindingHandlers.map = {
-
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        this.mapObj = ko.utils.unwrapObservable(valueAccessor());
-        var latLng = new google.maps.LatLng(
-            ko.utils.unwrapObservable(mapObj.lat),
-            ko.utils.unwrapObservable(mapObj.lng));
-        var options = { center: latLng,zoom: 5};
-
-        this.mapObj.googleMap = new google.maps.Map(element, options);
-        window.mapBounds = new google.maps.LatLngBounds();
-    }
-
-};
-
-function MapViewModel() {
-    var self = this;
-    self.centerMap = ko.observable({
-        lat: ko.observable(55),
-        lng: ko.observable(11)});
-
-    this.markers = ko.observableArray([]);
-
-    this.locationLat = ko.observable("");
-
-    this.mapSearch = function(){
-        var service = new google.maps.places.PlacesService(this.mapObj.googleMap);
-        var request = {
-            query: this.locationLat()
-        };
-        service.textSearch(request, callback);
-    };
-
-    var Marker = function(placeData, map) {
-        var googleMarker;
-
-        var lat = placeData.geometry.location.lat();  // latitude from the place service
-        var lon = placeData.geometry.location.lng();  // longitude from the place service
-        var name = placeData.formatted_address;   // name of the place from the place service
-
-        this.title = ko.observable(name);
-        this.lat  = ko.observable(lat);
-        this.lon  = ko.observable(lon);
-
-        googleMarker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, lon),
-            animation: google.maps.Animation.DROP,
-            map: map
-        });
-
-        this.isVisible = ko.observable(false);
-
-        this.isVisible.subscribe(function(visibility) {
-            if (visibility) {
-                googleMarker.setMap(map)
-            } else {
-                googleMarker.setMap(null);
-            }
-        });
-
-        this.isVisible(true);
-        return this;
-    };
-
-    this.callback = function(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            var pin = new Marker(results[0], this.mapObj.googleMap);
-            this.markers.push(pin);
-        }
-    };
-
-    this.toggle = ko.observable(true);
-    this.titleSearch = ko.observable("");
+$(function(){
 
     ko.observableArray.fn.filterByProperty = function(propName, matchValue) {
         return ko.pureComputed(function() {
@@ -88,35 +16,117 @@ function MapViewModel() {
         }, this);
     };
 
-    this.filteredMarkers = this.markers.filterByProperty("visible", true);
-
-    this.markerToggler = function(text) {
-        for(var marker in this.markers())
+    var Map = {
+        setMap: function(valueAccessor, element)
         {
-            var markerObj = this.markers()[marker];
-            markerObj.isVisible(markerObj.title().match(text) != null);
+            var mapObj = ko.utils.unwrapObservable(valueAccessor());
+            var latLng = new google.maps.LatLng(
+                ko.utils.unwrapObservable(mapObj.lat),
+                ko.utils.unwrapObservable(mapObj.lng));
+
+            var options = { center: latLng,zoom: 5};
+            this.map = new google.maps.Map(element, options);
+            window.mapBounds = new google.maps.LatLngBounds();
+            this.searchService = new google.maps.places.PlacesService(this.map);
+        },
+        getMap: function(){
+            return this.map;
+        },
+        getSearchService: function(){
+            return this.searchService;
+        },
+        getMapCenter: function() {
+            return {
+                lat: ko.observable(55),
+                lng: ko.observable(11)
+            }
         }
     };
 
-    this.toggleMarkers = function() {
-        this.toggle(!this.toggle());
-        if(!this.toggle())
-            this.markerToggler(null);
-        else
-            this.markerToggler("");
+    var Marker = function(placeData, map) {
+        var googleMarker;
+        var lat = placeData.geometry.location.lat();  // latitude from the place service
+        var lon = placeData.geometry.location.lng();  // longitude from the place service
+        var name = placeData.formatted_address;   // name of the place from the place service
+        var self = this;
+        self.title = ko.observable(name);
+
+        googleMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lon),
+            animation: google.maps.Animation.DROP,
+            map: map
+        });
+
+        self.isVisible = ko.observable(false);
+
+        self.isVisible.subscribe(function (visibility) {
+            if (visibility) {
+                googleMarker.setMap(map)
+            } else {
+                googleMarker.setMap(null);
+            }
+        });
+
+        self.isVisible(true);
     };
 
-    this.toggleMarkersForSearch = function() {
-        var val = $('#searchCrit').val();
-        var text = new RegExp(val, 'i');
-        this.markerToggler(text);
-    };
+    function MapViewModel() {
+        var self = this;
 
-    this.filterMarkers = function() {
-        this.titleSearch($('#searchCrit').val());
-    };
-}
+        ko.bindingHandlers.mapBinder = {
+            init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+                Map.setMap(valueAccessor, element);
+            }
+        };
 
+        self.centerMap = ko.observable(Map.getMapCenter());
 
+        self.markers = ko.observableArray([]);
+        self.locationLat = ko.observable("");
 
-ko.applyBindings(MapViewModel);
+        self.addMarkerOnMap = function(){
+            var service = Map.getSearchService();
+            var request = {
+                query: self.locationLat()
+            };
+            service.textSearch(request, addMarkerCallback);
+        };
+
+        function addMarkerCallback(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                var pin = new Marker(results[0], Map.getMap());
+                self.markers.push(pin);
+            }
+        }
+
+        self.toggle = ko.observable(true);
+        self.titleSearch = ko.observable("");
+
+        self.filteredMarkers = this.markers.filterByProperty("visible", true);
+
+        self.markerToggler = function(text) {
+            for(var marker in self.markers())
+            {
+                var markerObj = self.markers()[marker];
+                markerObj.isVisible(markerObj.title().match(text) != null);
+            }
+        };
+
+        self.toggleAllMarkers = function() {
+            self.toggle(!this.toggle());
+            if(!self.toggle())
+                self.markerToggler(null);
+            else
+                self.markerToggler("");
+        };
+
+        this.toggleMarkersForSearch = function(value) {
+            var text = new RegExp(value, 'i');
+            self.markerToggler(text);
+        };
+
+        self.titleSearch.subscribe(self.toggleMarkersForSearch);
+    }
+
+    ko.applyBindings(MapViewModel);
+});
--- client/dist/utility.js
+++ client/dist/utility.js
@@ -1,32 +1,6 @@
 /**
  * Created by ssingh on 7/30/15.
  */
-function createMapMarker(placeData, map){
-
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-        map: map,
-        position: placeData.geometry.location,
-        title: name
-    });
-
-    createInfoWindow(name, map, marker);
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-    return marker;
-}
 
 function createInfoWindow (content, map, marker) {
     // infoWindows are the little helper windows that open when you click
